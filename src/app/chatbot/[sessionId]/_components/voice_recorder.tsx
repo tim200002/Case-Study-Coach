ff050895157e1e3a79cfdb0440ada037d69ce03d
Tim Lindenau <tim.lindenau@outlook.de>
2023-11-01 13:46:08 +0900

    implement reocding pop up

--- src/app/chatbot/[sessionId]/_components/voice_recorder.tsx
+++ src/app/chatbot/[sessionId]/_components/voice_recorder.tsx
@@ -9,13 +9,24 @@ export const VoiceRecorderButton = () => {
   };
 
   return (
-    <button
-      onClick={handleRecord}
-      className={`relative m-2 rounded-full bg-blue-500 p-4 ${
-        isRecording ? "animate-pulse" : ""
-      }`}
-    >
-      <IconMicrophone size={24} color="white" />
-    </button>
+    <div className="relative flex justify-center">
+      {isRecording && (
+        <div 
+          className="absolute bottom-full mb-2 bg-white border p-4 rounded shadow-lg max-w-md max-h-40 overflow-y-auto"
+          style={{ transform: 'translateX(-50%)', left: '50%' }}
+        >
+          <p>Live Transcript:</p>
+          <p>...fsfjksldfjlsdkjfklsdjfklsdjfklsjdfklsjdfl kjsdklfjksldjfklfsfnsknfsdkfdsjfkjsdlkfjsdkljfklsdjflksdj fkljskdjfklsjfklsjfkjsdlfjsdkfjsdklfjslkfjslfkdjfklsdjf lsjfkls fsfjksldfjlsdkjfklsdjfklsdjfklsjdfklsjdfl kjsdklfjksldjfklfsfnsknfsdkfdsjfkjsdlkfjsdkljfklsdjflksdj fkljskdjfklsjfklsjfkjsdlfjsdkfjsdklfjslkfjslfkdjfklsdjf lsjfkls</p>
+        </div>
+      )}
+      <button
+        onClick={handleRecord}
+        className={`m-2 rounded-full bg-blue-500 p-4 ${
+          isRecording ? "animate-pulse" : ""
+        }`}
+      >
+        <IconMicrophone size={24} color="white" />
+      </button>
+    </div>
   );
 };
