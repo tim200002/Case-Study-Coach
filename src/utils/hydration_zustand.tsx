b468c246f328ee2d2974d43d1737f2c1b618ccb5
Tim Lindenau <tim.lindenau@outlook.de>
2023-11-08 21:47:49 +0900

    add documentation

--- src/utils/hydration_zustand.tsx
+++ src/utils/hydration_zustand.tsx
@@ -1,5 +1,12 @@
 import { ReactNode, useEffect, useState } from "react";
 
+/**
+ * This component is necessary for when using the persistent Zustand store.
+ * Otherwise there will be errors with hydration whenever the state of the store is not the
+ * same as the initial hydrated one.
+ *
+ * For more info see: https://medium.com/intelliconnect-engineering/fixing-hydration-issues-in-next-js-and-zustand-a-simple-solution-bd0a8deff6cc
+ */
 const HydrationZustand = (props: { children: ReactNode }) => {
   const [isHydrated, setIsHydrated] = useState(false);
 
