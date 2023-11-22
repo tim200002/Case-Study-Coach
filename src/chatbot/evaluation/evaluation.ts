78ac97b3b9451449eec4159accda39f943f8a916
sigurdthorlund <sigurd.thorlund@gmail.com>
2023-11-22 18:59:05 +0900

    Evaluation

---
+++ src/chatbot/evaluation/evaluation.ts
@@ -0,0 +1,25 @@
+// Two functions
+
+
+// Generate evaluation
+// - One big function for creating the evaluation
+
+
+
+// Subfunctions
+
+
+
+
+// Evaluate component function
+
+
+
+// Evaluate
+
+
+
+
+// Call LLM With Wrapper and wrapper should be parsed function
+
+
