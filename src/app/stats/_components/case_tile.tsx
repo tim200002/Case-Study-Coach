07cd4ec5e7f9c052bd1aea5ab8e3fee936601013
sigurdthorlund <sigurd.thorlund@gmail.com>
2023-11-22 16:59:38 +0900

    Adding dates to the case tiles

--- src/app/stats/_components/case_tile.tsx
+++ src/app/stats/_components/case_tile.tsx
@@ -12,8 +12,11 @@ const CaseTile = (props: {
   title: string;
   isCompleted: boolean;
   sessionId: number;
+  createdAt: Date;
 }) => {
-  const { title, isCompleted, sessionId } = props;
+  const { title, isCompleted, sessionId, createdAt } = props;
+
+  console.log("CreatedAt", createdAt);
 
   const textContent = isCompleted ? "Completed" : "In Progress";
   const textColor = isCompleted ? "text-green-500" : "text-yellow-500";
@@ -38,6 +41,9 @@ const CaseTile = (props: {
   return (
     <div className="m-2 flex flex-col rounded border p-6 shadow-md">
       <h1 className="text-l font-semibold">{title}</h1>
+      <p className="text-sm text-gray-500">
+        Started on {new Date(createdAt).toLocaleDateString()}
+      </p>
       <p className={textColor}>{textContent}</p>
       <div className="flex flex-row">
         <div className="grow" />
