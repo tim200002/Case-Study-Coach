18b9e5984517bf0ddaa99f51ca1718c90fbf9f29
sigurdthorlund <sigurd.thorlund@gmail.com>
2023-11-23 14:57:39 +0900

    removed import

--- src/app/chatbot/[sessionId]/_components/CaseCompleted.tsx
+++ src/app/chatbot/[sessionId]/_components/CaseCompleted.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { evaluations } from "~/server/db/schema";
 
 const CaseCompleted = (props: { sessionId: number }) => {
   const { sessionId } = props;
