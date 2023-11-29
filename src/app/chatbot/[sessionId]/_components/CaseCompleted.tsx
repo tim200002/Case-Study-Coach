74a5d4deacc6f0065774414544b26161b1ea5ec4
Tim Lindenau <tim.lindenau@outlook.de>
2023-11-29 21:01:24 +0900

    bug fix for deployment

--- src/app/chatbot/[sessionId]/_components/CaseCompleted.tsx
+++ src/app/chatbot/[sessionId]/_components/CaseCompleted.tsx
@@ -1,37 +1,22 @@
 "use client";
 
+//ToDo REbuild as server component
+
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { api } from "~/trpc/react";
 
 const CaseCompleted = (props: { sessionId: number }) => {
   const { sessionId } = props;
-  const [evaluationId, setEvaluationId] = useState<number | null>(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchEvaluationId = async () => {
-      try {
-        const evaluation = await api.case.getEvaluation.query({
-          sessionId: sessionId,
-        });
-        setEvaluationId(evaluation!.id);
-      } catch (e) {
-        throw e;
-      } finally {
-        console.log("done");
-        setLoading(false);
-      }
-    };
-
-    fetchEvaluationId();
+  const { data: evaluation, isLoading } = api.case.getEvaluation.useQuery({
+    sessionId: sessionId,
   });
 
   return (
     <div className="m-2 flex flex-col items-center">
       <h1 className="m-4 text-4xl font-bold">Case Completed</h1>
-      {loading && <h1>Loading...</h1>}
-      {!loading && <h1>{evaluationId}</h1>}
+      {isLoading && <h1>Loading...</h1>}
+      {!isLoading && <h1>{evaluation!.id}</h1>}
       <Link href={`/evaluation/${sessionId}`}>
         <p className="inline-block rounded bg-green-500 px-4 py-2 text-center font-bold text-white hover:bg-blue-700">
           Go To Evaluation
