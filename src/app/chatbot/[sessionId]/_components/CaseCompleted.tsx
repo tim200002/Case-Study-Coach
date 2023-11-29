955215632600b6b1d2c20fc1255877f2a19bf461
Tim Lindenau <tim.lindenau@outlook.de>
2023-11-29 20:57:07 +0900

    add bugfix for deployment

--- src/app/chatbot/[sessionId]/_components/CaseCompleted.tsx
+++ src/app/chatbot/[sessionId]/_components/CaseCompleted.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { api } from "~/trpc/react";
 
 const CaseCompleted = (props: { sessionId: number }) => {
   const { sessionId } = props;
